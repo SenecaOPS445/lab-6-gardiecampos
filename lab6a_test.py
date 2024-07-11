0a4744cf2edc0160ab726a06c715dfb32df04b4c
Lou Gardie Campos <lgcampos@myseneca.ca>
2024-07-11 19:40:23 -0400

    completed lab6.

---
+++ lab6a_test.py
@@ -0,0 +1,12 @@
+import sys
+
+from lab6a import Student
+
+student1 = Student('Jack', 931686102)
+
+student1.addGrade('ops535', 2.0)
+
+student1.addGrade('win310', 0.0)
+
+student1.displayStudent()
+# Will print: 'Student Name: Jack\nStudent Number: 931686102'
